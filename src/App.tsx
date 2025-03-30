5ddb3e8a1990e997f96f455eaa4ffe67e2cdd7a9
Renx24 <remerida@hotmail.com>
2025-03-30 22:13:51 +0300

    add bg-image, adjusted styles, worked on main logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import MemoryCard from "./components/MemoryCard.tsx";
 
 function App() {
   const [selectedMovies, setSelectedMovies] = useState<any[]>([]);
+  const [clickedMovies, setClickedMovies] = useState<any[]>([]);
+  const [highScore, setHighScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
 
   const checkImage = (url: string): Promise<boolean> => {
     return new Promise((resolve) => {
@@ -50,20 +53,59 @@ function App() {
     getData();
   }, []);
 
+  const resetGame = () => {
+    setClickedMovies([]);
+    setCurrentScore(0);
+  };
+
+  const continueGame = (title: string) => {
+    setClickedMovies([...clickedMovies, title]);
+    setCurrentScore(currentScore + 1);
+  };
+
+  const handleCardClick = (title: string) => {
+    if (clickedMovies.includes(title)) {
+      alert("Game over! You clicked the same card twice.");
+      if (currentScore > highScore) setHighScore(currentScore);
+      resetGame();
+    } else {
+      continueGame(title);
+    }
+    getData();
+  };
+
   return (
-    <div className="memory-grid">
-      {selectedMovies.length > 0 ? (
-        selectedMovies.map((movie, index) => (
-          <MemoryCard
-            key={index}
-            title={movie?.title}
-            image={movie?.posterURL}
-          />
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <>
+      <div className="text-section">
+        <div className="instructions">
+          <h1>Memory Card Game</h1>
+          <p>Don't click on a card that you have already clicked on!</p>
+        </div>
+        <div className="scores">
+          <p>
+            High Score: <span className="highscore">{highScore}</span>
+          </p>
+          <p>
+            Score: <span className="current-score">{currentScore}</span>
+          </p>
+        </div>
+      </div>
+
+      <div className="memory-grid">
+        {selectedMovies.length > 0 ? (
+          selectedMovies.map((movie, index) => (
+            <MemoryCard
+              key={index}
+              title={movie?.title}
+              image={movie?.posterURL}
+              onClick={() => handleCardClick(movie.title)}
+            />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </>
   );
 }
 
--- src/components/MemoryCard.tsx
+++ src/components/MemoryCard.tsx
@@ -1,14 +1,18 @@
 export default function MemoryCard({
   image,
   title,
+  onClick,
 }: {
   image: string;
   title: string;
+  onClick: () => void;
 }) {
   return (
-    <div className="memory-card">
+    <div className="memory-card" onClick={onClick}>
       <img src={image} alt={title} />
-      <p>{title}</p>
+      <div className="memory-card-title">
+        <p>{title}</p>
+      </div>
     </div>
   );
 }
